79e71e5e72c56c572a162b33c5b2b93841ae857e
clarisse-oyharcabal <clarisse.oyharcabal@laplateforme.io>
2025-01-30 22:50:14 +0100

    <image>: load box image

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ display.set_caption('Corn Ninja')
 # Load images
 background_main_menu = image.load('assets/img/background_main_menu.jpg').convert()
 background_play = image.load('assets/img/background_play.jpg').convert()
+seau = image.load('assets/img/seau.jpg').convert_alpha()
 corn_yellow = image.load('assets/img/corn_yellow.jpg').convert()
 corn_red = image.load('assets/img/corn_red.jpg').convert()
 corn_blue = image.load('assets/img/corn_blue.jpg').convert()
 corn_green = image.load('assets/img/corn_green.jpg').convert()
-bomb = image.load('assets/img/bomb.gif').convert()
+bomb = image.load('assets/img/bomb.png').convert()
 ice = image.load('assets/img/ice.png').convert_alpha()
 life = image.load('assets/img/life.png').convert_alpha()
 button_play = image.load('assets/img/button_play.jpg').convert()
@@ -28,6 +29,7 @@ button_lang = image.load('assets/img/button_lang.png').convert()
 # Resize images if necessary
 background_main_menu = transform.scale(background_main_menu, (window_width, window_height))
 background_play = transform.scale(background_play, (window_width, window_height))
+seau = transform.scale(seau, (110, 110))
 corn_yellow = transform.scale(corn_yellow, (50, 50))
 corn_red = transform.scale(corn_red, (50, 50))
 corn_blue = transform.scale(corn_blue, (50, 50))
@@ -65,7 +67,7 @@ while running:
         draw_language_menu(window, background_main_menu)
 
     elif state == 3:  # Game state
-        draw_game(window, background_play, objects, special_objects_easy, corn_yellow, corn_red, corn_blue, corn_green, bomb, ice, life, window_width, window_height)
+        draw_game(window, background_play, seau, objects, special_objects_easy, corn_yellow, corn_red, corn_blue, corn_green, bomb, ice, life, window_width, window_height)
         # Spawn new objects randomly
         if randint(0, 100) < 1:  # 2% chance to spawn an object each frame
             spawn_corn(window_height, objects)
