0f6f6186dec947f03474b07d790a205f297be35d
clarisse-oyharcabal <clarisse.oyharcabal@laplateforme.io>
2025-02-03 03:39:22 +0100

    <feat>: add level hard more objects spawn

--- functions/button_events.py
+++ functions/button_events.py
@@ -20,6 +20,9 @@ def button_events(state, mouse_pos):
             if event.key == K_e:  # Touche 'E' pour jouer en mode Easy
                 if state == 1:  # Menu de difficulté
                     state = 3  # Démarrer en mode Easy
+            if event.key == K_h:  # Touche 'E' pour jouer en mode Easy
+                if state == 1:  # Menu de difficulté
+                    state = 4  # Démarrer en mode Easy
 
                     
         if event.type == MOUSEBUTTONDOWN and event.button == 1:  # Clic gauche
@@ -41,7 +44,7 @@ def button_events(state, mouse_pos):
                     state = 3  # Démarrer en mode Easy
 
                 if hard_button_rect.collidepoint(mouse_pos):
-                    pass  # Mode Hard (à implémenter)
+                    state = 4  # Mode Hard 
 
             elif state == 2:  # Menu des langues
                 english_button_rect = pygame.Rect(90, 430, 100, 50)
